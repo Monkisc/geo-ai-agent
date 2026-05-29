8ce73cce0be428e2b953ca24df5fde190b6ccd1f
Monkisc <juanpacobaper@gmail.com>
2026-05-29 00:51:56 -0500

    fix: condicional para load_dotenv para compatibilidad con Cloud Run

--- agents/geo_agent.py
+++ agents/geo_agent.py
@@ -1,17 +1,18 @@
 from google import genai
 import os
 from dotenv import load_dotenv
-
 from services.google_maps_service import search_places
 
-load_dotenv()
+# 1. Cargar el entorno local solo si existe el archivo
+if os.path.exists(".env"):
+    load_dotenv()
 
+# 2. Inicializar el cliente (Alineado al borde izquierdo, fuera del 'if')
 client = genai.Client(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
 def ask_agent(user_query, page_token=None):
-
     places = search_places(user_query, page_token)
 
     prompt = f"""
